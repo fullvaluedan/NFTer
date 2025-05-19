6113c6440d40ad6df25ff1321e44b2c3f3373bf5
Augustin Chan <aug@iterative.day>
2025-05-19 18:18:12 +0800

    Refactor import paths and update Next.js configuration
    - Added a new jsconfig.json file to define base URL and path aliases for improved module resolution.
    - Updated next.config.js to utilize experimental turbo features for aliasing.
    - Modified import statements in several component files to reflect the new relative paths.

--- nfter-next/next.config.js
+++ nfter-next/next.config.js
@@ -3,12 +3,12 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(process.cwd(), 'src'),
-    };
-    return config;
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        '@': './src',
+      },
+    },
   },
 };
 
